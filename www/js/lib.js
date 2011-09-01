11ee4487b46d9225ddcb703d8a83a95c2b0e5003
Jaime Bunzli <jpbunzli@gmail.com>
2011-08-31 21:48:06 -0300

    primera version aplicacion

--- www/js/lib.js
+++ www/js/lib.js
@@ -1,1 +1,64 @@
-// JavaScript Document
+
+//thx @elroyjetson for the code example
+
+var map;
+
+// When map page opens get location and display map
+$('.page-map').live("pagecreate", function() {
+
+	//boston :)
+	var lat = 42.35843,
+		lng = -71.059773;
+
+	//try to get GPS coords
+	if( navigator.geolocation ) {
+
+		//redirect function for successful location
+		function gpsSuccess(pos){
+			if( pos.coords ){
+				lat = pos.coords.latitude;
+				lng = pos.coords.longitude;
+			}
+			else{
+				lat = pos.latitude;
+				lng = pos.longitude;
+			}
+		}
+
+		function gpsFail(){
+			//Geo-location is supported, but we failed to get your coordinates. Workaround here perhaps?
+		}
+
+		navigator.geolocation.getCurrentPosition(gpsSuccess, gpsFail, {enableHighAccuracy:true, maximumAge: 300000});
+	}
+
+	/*
+	if not supported, you might attempt to use google loader for lat,long
+	$.getScript('http://www.google.com/jsapi?key=YOURAPIKEY',function(){
+		lat = google.loader.ClientLocation.latitude;
+		lng = google.loader.ClientLocation.longitude;
+	});
+	*/
+
+	var latlng = new google.maps.LatLng(lat, lng);
+	var myOptions = {
+		zoom: 10,
+		center: latlng,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById("map-canvas"),myOptions);
+
+    navigator.geolocation.getCurrentPosition(geolocationSuccess);
+
+});
+
+
+function geolocationSuccess() {
+    var latlng = new google.maps.LatLng(-33.398647,-70.581193);
+    map.setCenter(latlng);
+}
+
+function geolocationError() {
+    var latlng = new LatLng(-33.398647,-70.581193);
+    map.setCenter(latlng);
+}
